93ad5652615db0b101116988b07cd7385b073a1e
Thinh Vo <thinhvkit@gmail.com>
2019-09-24 16:20:01 +0700

    add action bottom

--- components/Close.js
+++ components/Close.js
@@ -1,34 +1,73 @@
-import React from 'react';
-import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import React from "react";
+import { StyleSheet, TouchableOpacity, View, Text } from "react-native";
 
-export const Close = ({ onPress }) => {
-	return (
-		<TouchableOpacity activeOpacity={1} onPress={onPress} style={CloseStyles.closeButton}>
-			<View style={[ CloseStyles.iconLine, { transform: [ { rotateZ: '45deg' } ] } ]} />
-			<View style={[ CloseStyles.iconLine, { transform: [ { rotateZ: '135deg' } ] } ]} />
-		</TouchableOpacity>
-	);
+export const Close = ({ onBack, onNext, caption }) => {
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        backgroundColor: "red",
+        position: "absolute",
+        bottom: 150,
+        left: 40,
+        right: 40,
+        display: "flex",
+        justifyContent: "center",
+		alignItems: "center",
+		zIndex: 3,
+      }}
+    >
+      <TouchableOpacity onPress={onBack} style={CloseStyles.closeButton}>
+        <View
+          style={[CloseStyles.iconLine, { transform: [{ rotateZ: "45deg" }] }]}
+        />
+        <View
+          style={[CloseStyles.iconLine, { transform: [{ rotateZ: "135deg" }] }]}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={onNext} style={CloseStyles.nextButton}>
+        <Text style={{ color: "white", alignSelf: "center", fontSize: 20 }}>
+          {caption}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const CloseStyles = StyleSheet.create({
-	closeButton: {
-		width: 30,
-		height: 30,
-		borderRadius: 15,
-		position: 'absolute',
-		right: 20,
-		top: 20,
-		backgroundColor: '#e2e2e2',
-		zIndex: 3,
-		display: 'flex',
-		justifyContent: 'center',
-		alignItems: 'center'
+  closeButton: {
+    position: "absolute",
+    left: 0,
+    width: 50,
+    height: 50,
+    borderRadius: 12,
+    backgroundColor: "white",
+    justifyContent: "center",
+    shadowColor: "black",
+    shadowOffset: {
+      width: 1,
+      height: 1
 	},
-	iconLine: {
-		position: 'absolute',
-		width: 18,
-		height: 2,
-		borderRadius: 2,
-		backgroundColor: 'white'
-	}
+	elevation: 1,
+    shadowOpacity: 0.7,
+    shadowRadius: 1.0
+  },
+  nextButton: {
+    position: "absolute",
+    right: 0,
+    width: 250,
+    height: 50,
+    borderRadius: 12,
+    position: "absolute",
+    justifyContent: "center",
+    backgroundColor: "#3370bc"
+  },
+  iconLine: {
+    position: "absolute",
+    width: 18,
+    height: 2,
+    borderRadius: 2,
+    backgroundColor: "gray",
+    alignSelf: "center"
+  }
 });
